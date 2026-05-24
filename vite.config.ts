1ebceecd923d9e03f0fc1e8b108cf25d1e6216d3
Jacek Pietrzyk <jacpie3k@gmail.com>
2026-05-24 18:03:35 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     tailwindcss(),
   ],
   resolve: {
