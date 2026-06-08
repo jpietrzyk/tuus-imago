441eeee57e79509ed5051bdade63f4abf50db9e3
Jacek Pietrzyk <jacpie3k@gmail.com>
2026-06-08 23:36:00 +0200

    Done (#379)

--- src/components/image-uploader/painting-size.ts
+++ src/components/image-uploader/painting-size.ts
@@ -1,4 +1,4 @@
-export type PaintingSizeIndex = 0 | 1 | 2 | 3 | 4;
+export type PaintingSizeIndex = 0 | 1 | 2 | 3 | 4 | 5;
 
 export type PaintingShape = "square" | "rectangular";
 
@@ -16,6 +16,8 @@ const SQUARE_OPTIONS: PaintingSizeOption[] = [
   { key: 1, label: "50 x 50", widthCm: 50, heightCm: 50 },
   { key: 2, label: "60 x 60", widthCm: 60, heightCm: 60 },
   { key: 3, label: "80 x 80", widthCm: 80, heightCm: 80 },
+  { key: 4, label: "100 x 100", widthCm: 100, heightCm: 100 },
+  { key: 5, label: "120 x 120", widthCm: 120, heightCm: 120 },
 ];
 
 const RECTANGULAR_OPTIONS: PaintingSizeOption[] = [
@@ -32,9 +34,10 @@ const SIZE_SCALE_MAP: Record<PaintingSizeIndex, number> = {
   2: 1.0,
   3: 1.333,
   4: 1.667,
+  5: 2.0,
 };
 
-export const ALL_PAINTING_SIZE_INDICES: PaintingSizeIndex[] = [0, 1, 2, 3, 4];
+export const ALL_PAINTING_SIZE_INDICES: PaintingSizeIndex[] = [0, 1, 2, 3, 4, 5];
 
 export function getPaintingSizeOptions(
   shape: PaintingShape,
